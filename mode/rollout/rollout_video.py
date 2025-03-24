c5b66e12197c12bd551fedcd1ffaabf6de822e9c
thomas-roth <uruox@student.kit.edu>
2025-03-24 19:37:52 +0100

    rework itrap eval

--- mode/rollout/rollout_video.py
+++ mode/rollout/rollout_video.py
@@ -255,9 +255,9 @@ class RolloutVideo:
 
             tag = tag.replace("/", "_")
             if save_as_video:
-                filename = str(self.save_dir / f"{tag}_{global_step}.mp4")
+                filename = str(self.save_dir / f"{tag}-{global_step}.mp4")
             else:
-                filename = self.save_dir / f"{tag}_{global_step}.gif"
+                filename = self.save_dir / f"{tag}-{global_step}.gif"
             if save_as_video:
                 clip.write_videofile(filename, codec='libx264', bitrate="5000k")  # You can adjust the bitrate as needed
             else:
